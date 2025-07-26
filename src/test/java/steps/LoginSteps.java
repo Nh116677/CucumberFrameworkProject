c37c659325980a5b45377a8e697a821714e7d0c2
Nhu Nguyen <nhu116677@gmail.comgit config --global user.name Nh116677git config --global user.email nhu116677@gmail.com>
2025-07-26 15:29:12 -0400

    First Commit

--- src/test/java/steps/LoginSteps.java
+++ src/test/java/steps/LoginSteps.java
@@ -3,6 +3,7 @@ package steps;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
+import org.junit.Assert;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
@@ -14,8 +15,8 @@ import java.time.Duration;
 
 public class LoginSteps extends CommonMethods {
 
-    WebDriver driver;
-    LoginPage loginPage = new LoginPage()
+    //WebDriver driver;
+    //LoginPage loginPage = new LoginPage()
 
 
     @Given("the user is on the HRMs login page")
@@ -46,19 +47,22 @@ public class LoginSteps extends CommonMethods {
     @Then("the user should see an error message stating Username cannot be empty.")
     public void the_user_should_see_an_error_message_stating_username_cannot_be_empty() {
        // Locate the error message text
-        String actualEquals(,)
+        WebElement errorMessage = driver.findElement(By.id(""));
+
+        //Verify the error message text
+        String actualMessage = errorMessage.getText();
         loginPage.usernameField.clear("");
+        System.out.println("Username cannot be empty");
 
     }
 
 
-    io.cucumber.junit.UndefinedStepException: The step 'the user is on the HRMs login page' and 4 other step(s) are undefined.
-    You can implement these steps using the snippet(s) below:
-
     @Given("the user is on the HRMs login page")
     public void the_user_is_on_the_hr_ms_login_page() {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.get("http://hrm.syntaxtechs.net/humanresources/symfony/web/index.php/auth/login");
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
     }
     @When("the user enters a valid username in the username field")
     public void the_user_enters_a_valid_username_in_the_username_field() {
@@ -82,13 +86,12 @@ public class LoginSteps extends CommonMethods {
     }
 
 
-    io.cucumber.junit.UndefinedStepException: The step 'the user is on the HRMs login page' and 4 other step(s) are undefined.
-    You can implement these steps using the snippet(s) below:
-
     @Given("the user is on the HRMs login page")
     public void the_user_is_on_the_hr_ms_login_page() {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.get("http://hrm.syntaxtechs.net/humanresources/symfony/web/index.php/auth/login");
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
     }
     @When("the user enters an incorrect username in the username field")
     public void the_user_enters_an_incorrect_username_in_the_username_field() {
@@ -102,38 +105,47 @@ public class LoginSteps extends CommonMethods {
     }
     @When("the user clicks the Login button")
     public void the_user_clicks_the_login_button() {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
+        click(loginPage.loginButton);
     }
     @Then("the system should display an error message stating Invalid credentials.")
     public void the_system_should_display_an_error_message_stating_invalid_credentials() {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
+
+
     }
 
-
-    io.cucumber.junit.UndefinedStepException: The step 'the user is on the HRMs login page' and 3 other step(s) are undefined.
-    You can implement these steps using the snippet(s) below:
 
     @Given("the user is on the HRMs login page")
     public void the_user_is_on_the_hr_ms_login_page() {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.get("http://hrm.syntaxtechs.net/humanresources/symfony/web/index.php/auth/login");
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
     }
     @When("the user clicks the Login button without entering any credentials")
     public void the_user_clicks_the_login_button_without_entering_any_credentials() {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
+        click(loginPage.loginButton);
     }
     @Then("the error message Username cannot be empty. should be displayed near the username field")
     public void the_error_message_username_cannot_be_empty_should_be_displayed_near_the_username_field() {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
+        //Locate the error message element
+        WebElement userNameErrorMsg = driver.findElement(By.id(""));
+
+        //Verify the error message text
+        String userNameActualMessage = userNameErrorMsg.getText();
+        String userNameExpectedMessage = "Username cannot be empty.";
+        Assert.assertEquals("Error message did not match",userNameExpectedMessage, userNameActualMessage);
+
     }
     @Then("the error message Password cannot be empty. should be displayed near the password field")
     public void the_error_message_password_cannot_be_empty_should_be_displayed_near_the_password_field() {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
+        //Locate the error message element
+        WebElement passwordErrorMsg = driver.findElement(By.id(""));
+
+        //Verify the error message text
+        String passwordActualMessage = passwordErrorMsg.getText();
+        String passwordExpectedMessage = "Username cannot be empty.";
+        Assert.assertEquals("Error message did not match",passwordExpectedMessage, passwordActualMessage);
+
     }
 
 
@@ -147,23 +159,25 @@ public class LoginSteps extends CommonMethods {
     }
     @When("the user corrects the username field")
     public void the_user_corrects_the_username_field() {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
+        loginPage.usernameField.sendKeys("admin");
     }
     @When("the user corrects the password field")
     public void the_user_corrects_the_password_field() {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
+        loginPage.passwordField.sendKeys("Hum@nhrm123");
     }
     @When("the user clicks the Login button")
     public void the_user_clicks_the_login_button() {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
+        click(loginPage.loginButton);
     }
     @Then("the user should be redirected to the HRMs dashboard")
     public void the_user_should_be_redirected_to_the_hr_ms_dashboard() {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
+        //validating the existence of the element
+        Assert.assertTrue(loginPage.welcomeMessageLoc.isDisplayed());
+
+        String value = loginPage.welcomeMessageLoc.getText();
+        //validating the exact message on the element
+        Assert.assertEquals("Welcome Admin", value);
+        System.out.println("Test passed");
     }
 
 
